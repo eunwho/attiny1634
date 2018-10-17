08d13d1d322aeb0ea97fdef65300d04a83ec660c
eunwho <eunwho@naver.com>
2018-10-17 16:35:53 +0900

    checked 0x51 0x61 0x71

--- GccApplication1/main.c
+++ GccApplication1/main.c
@@ -11,6 +11,34 @@
 //#define MODE_MEASURE_CURRENT
 //#define MODE_MEASURE_PRESSURE
 
+//#define	READ_ADC	1
+
+//#define ID_0x51		1
+#define ID_0x61		1
+//#define ID_0x71		1
+
+#if ID_0x51
+#define ID_ADDR		0x51
+#define ADC_CHAN	0
+#define SENS_HIGH	100.0
+#define SENS_LOW	0.0
+#define ADC_HIGH	255
+#define ADC_LOW		0
+#endif
+
+#if ID_0x61
+#define ID_ADDR		0x61
+#define ADC_CHAN	1
+#define SENS_HIGH	5.0
+#define SENS_LOW	0.0
+#define ADC_HIGH	255
+#define ADC_LOW		0
+#endif
+
+#if ID_0x71
+#define ID_ADDR		0x71
+#endif
+
 #define SET		1
 #define RESET	0
 #define HIGH	1
@@ -37,42 +65,11 @@
 #define RS485_TX_EN		DDRC |= RS485_EN;
 #define RS485_RX_EN		DDRC &= ~RS485_EN;
 
-//--- by skj 2018.10.13
-#define MODE_MEASURE_DC		'A'
-#define ID_CURRENT			'B'
-#define ID_PRESSURE			'C'
-#define ID_DC				'D'
-
 //--- sensor calc 
-#define I_HIGH			200.0
-#define I_LOW			0.0
-#define I_ADC_HIGH		1024
-#define I_ADC_LOW		0
+float sensScale;
+float sensOffset;
 
-#define VOLT_HIGH		500.0
-#define VOLT_LOW		0.0
-#define VOLT_ADC_HIGH	1024
-#define VOLT_ADC_LOW	0
-
-#define PRESS_HIGH		500.0
-#define PRESS_LOW		0.0
-#define PRESS_ADC_HIGH	1024
-#define PRESS_ADC_LOW	0
-
-float currentScale;
-float currentOffset;
-
-float voltScale;
-float voltOffset;
-
-float pressScale;
-float pressOffset;
-
-float sensCurrent;
-float sensVolt;
-float sensPress;
-
-unsigned long adcResult[3];
+unsigned int	adcResult;
 unsigned long adcCount = 0;
 unsigned long adcCheck = 0;
 unsigned int adcCh = 0;
@@ -117,31 +114,21 @@ void uart_puts(const char *s) {
 	}
 }
 
-void calcSensScaleOffset(){
-	float x1,x2,y2,y1;
-	
-	y2 = I_HIGH; y1 = I_LOW ; x2 = I_ADC_HIGH ; x1 = I_ADC_LOW;
-	currentScale = ( y2 - y1 ) / ( x2 - x1 );
-	currentOffset = ( y1 * x2 - y2 * x1 ) / ( x2 - x1 );
-	
-	y2 = VOLT_HIGH; y1 = VOLT_LOW ; x2 = VOLT_ADC_HIGH ; x1 = VOLT_ADC_LOW;
-	voltScale = ( y2 - y1 ) / ( x2 - x1 );
-	voltOffset = ( y1 * x2 - y2 * x1 ) / ( x2 - x1 );
-	
-	y2 = PRESS_HIGH; y1 = PRESS_LOW ; x2 = PRESS_ADC_HIGH ; x1 = PRESS_ADC_LOW;
-	pressScale = ( y2 - y1 ) / ( x2 - x1 );
-	pressOffset = ( y1 * x2 - y2 * x1 ) / ( x2 - x1 );
-}
-
-float calcSens(unsigned input, float scale, float offset){
-	float returnValue;
-	
-	returnValue = scale * input + offset;
-	return returnValue;
-}
 
 unsigned long adCheck;
 
+#if	(ID_0x51 | ID_0x61)
+void calcSensScaleOffset(){
+	float x1,x2,y2,y1;
+	
+	y2 = SENS_HIGH; y1 = SENS_LOW ; x2 = ADC_HIGH ; x1 = ADC_LOW;
+	sensScale = ( y2 - y1 ) / ( x2 - x1 );
+	sensOffset = ( y1 * x2 - y2 * x1 ) / ( x2 - x1 );
+}
+
+float calcSens(unsigned int arg1){
+	return sensScale * arg1 + sensOffset;
+}
 
 void Initial_ADC(void)
 {
@@ -151,19 +138,15 @@ void Initial_ADC(void)
 	ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0) | (1<<ADIE ) | (1 << ADSC) | (1 << ADEN) | (1<< ADATE);		// irq enabled
 	ADCSRB = (1<<ADLAR );	// ADCH, ADCL are left adjusted
 	DIDR0 = 0x38;
-	//DIDR0 = 0x08;
+	ADMUX = ADC_CHAN;	// AREF use, select adc1
 }
 
 ISR(ADC_vect)
 {
-	adcResult[adcCh] = ADCH ;
-
-	if(adcCh < 2 ) adcCh ++;
-	else			adcCh = 0;
-
-	ADMUX = adcCh;
+	adcResult = ADCH ;
 	ADCSRA |= (1<<ADSC);
 }
+#endif
 
 ISR(USART0_RX_vect)
 {
@@ -189,9 +172,7 @@ ISR(USART0_RX_vect)
 
 			gflagConnect = SET;
 			flagStx = RESET;
-			for( i = 0 ; i < 6 ; i++){
-				gUartTxBuffer[i] = gUartRxBuffer[i];
-			}
+			for( i = 0 ; i < 6 ; i++) gUartTxBuffer[i] = gUartRxBuffer[i];
 			gUartIndex = 0;
 		}
 	}
@@ -226,127 +207,50 @@ ISR(TIMER1_COMPA_vect)	//1msec
 	else gCounter++;
 }
 
+void rs485Send( ){
 
-
-void readDCVoltage(void)
-{
-	// 방화문 전압(1~5V) 읽기
-	// 30V -> 5V (1~5V)
-	// 10bit : 1024
-	float fAdc;
-	float fDustVolt;
-
-	ADMUX = 0;	// AREF use, select adc1
-	
-	ADCSRA |= (1 << ADSC);         // start ADC measurement
-	while (ADCSRA & (1 << ADSC) ); // wait till conversion complete
-	_delay_us(40); // delta time
-	
-	_delay_us(9680);	// sleep time
-	
-	fAdc = ADC;
-	fDustVolt = fAdc * (5.0 / 1024.0);
-	
-	gValue_DCVoltage = (unsigned char)(fDustVolt * 6.0); 
-	if(gValue_DCVoltage < 1) gValue_DCVoltage = 0;
-}
-
-void readDigitalPressure(void)
-{
-	// 디지털압력계(1~5V) 읽기
-
-	float fAdc;
-	float fDustVolt;
-	
-	ADMUX = 1;	// AREF use, select adc1
-	
-	ADCSRA |= (1 << ADSC);         // start ADC measurement
-	while (ADCSRA & (1 << ADSC) ); // wait till conversion complete
-	_delay_us(40); // delta time
-	
-	_delay_us(9680);	// sleep time
-	
-	fAdc = ADC;
-	fDustVolt = fAdc * (5.0 / 1024.0);
-	
-	gValue_DigitalPressure = (fDustVolt - 2.5) * 40.0;
-	
-	gValue_DigitalPressure *= 10;
-	if(gValue_DigitalPressure <= 0) gValue_DigitalPressure = 0;
-}
-
-void readCurrent(void)
-{
-	// 전류측정
-
-	float fAdc;
-	float fDustVolt;
-	
-	ADMUX = 0;	// AREF use, select adc0 : PA3
-	
-	ADCSRA |= (1 << ADSC);         // start ADC measurement
-	while (ADCSRA & (1 << ADSC) ); // wait till conversion complete
-	_delay_us(40); // delta time
-	
-	_delay_us(9680);	// sleep time
-	
-	fAdc = ADC;
-	fDustVolt = fAdc * (5.0 / 1024.0);
-	
-	gValue_Current = fDustVolt * 6.0;
-	
-}
-
-void txReply(){
-	int i;
-	RS485_RX_EN;
-	_delay_ms(10);
-
-	
-	for( i = 0 ; i < 6 ; i++){  
-		send_uart( gUartTxBuffer[i]);
-	}
-	_delay_ms(20);
-}
-void rs485SendInput(){
-	int i, input;
-	RS485_RX_EN;
-
-	input = ( PINC & ( 1 << PINC2 )) ? 2 : 0; 	
-	
-	gUartTxBuffer[0] = 0x02;
-	gUartTxBuffer[1] = 'D';
-	gUartTxBuffer[2] = input + 0x30;
-	gUartTxBuffer[3] = 0x30;
-	gUartTxBuffer[4] = 0x30;
-	gUartTxBuffer[5] = 0x30;
-	gUartTxBuffer[6] = 0x03;
-
-	_delay_ms(5);
-	for( i = 0 ; i < 7 ; i++){
-		send_uart( gUartTxBuffer[i]);
-	}
-	_delay_ms(10);
-}
-
-void rs485Send(char sensId, float fInput){
 	int i;
 	unsigned long input;
-	
-	input = (unsigned long)(fInput * 10);
-	//	if(gUartRxBuffer[1] == ID_CURRENT && gUartRxBuffer[2] == 'R'){
+
 	RS485_RX_EN;
 
-	gUartTxBuffer[0] = 0x02;
-	gUartTxBuffer[1] = sensId;
-	gUartTxBuffer[2] = ((unsigned char)(input / 1000))			+ 0x30;
-	gUartTxBuffer[3] = ((unsigned char)((input % 1000)/100))	+ 0x30;
-	gUartTxBuffer[4] = ((unsigned char)((input % 100 ) /10))	+ 0x30;
-	gUartTxBuffer[5] = (unsigned char)( (input % 100)%10 + 0x30);
-	gUartTxBuffer[6] = 0x03;
+#if ID_0x71	
 
-	_delay_ms(5);
-	for( i = 0 ; i < 7 ; i++){
+	char tmp;
+	
+	i = ( PINC & ( 1 << PINC2 )) ? 1 : 0;
+	
+	tmp = ( i ) ? 'C' : 'O' ;
+	
+	for( i = 3 ; i < 7 ; i++) gUartTxBuffer[i] = tmp;
+	
+	gUartTxBuffer[0] = 0x02;
+	gUartTxBuffer[1] = ID_ADDR;
+	gUartTxBuffer[2] = 'A';
+	gUartTxBuffer[7] = 0x03;
+#endif
+
+#if READ_ADC
+	input = (unsigned long)(adcResult * 10);
+#else
+	if (ID_ADDR == 0x51)		input = (unsigned long)(calcSens( adcResult) / 100);
+	else if(ID_ADDR == 0x61 )   input = (unsigned long)(calcSens(adcResult ) * 10);
+	else                        input = 1;	
+#endif
+
+#if (ID_0x51 | ID_0x61 )
+	gUartTxBuffer[0] = 0x02;
+	gUartTxBuffer[1] = ID_ADDR;
+	gUartTxBuffer[2] = 'A';	
+	gUartTxBuffer[3] = ((unsigned char)(input / 1000))			+ 0x30;
+	gUartTxBuffer[4] = ((unsigned char)((input % 1000)/100))	+ 0x30;
+	gUartTxBuffer[5] = ((unsigned char)((input % 100 ) /10))	+ 0x30;
+	gUartTxBuffer[6] = (unsigned char)( (input % 100)%10 + 0x30);
+	gUartTxBuffer[7] = 0x03;
+#endif
+
+	_delay_ms(7);
+	for( i = 0 ; i < 8 ; i++){
 		send_uart( gUartTxBuffer[i]);
 	}
 	_delay_ms(10);
@@ -354,16 +258,16 @@ void rs485Send(char sensId, float fInput){
 
 int main(void)
 {
-	char arg1;
-	float arg2;
 
 	//_delay_ms(500);
 	Initial_GPIO();
 	Initial_uart();
 	Initial_Timer();
+
+#if (ID_0x51 | ID_0x61)
 	Initial_ADC();
-	
 	calcSensScaleOffset();   // get scale and offset for calc sensor value
+#endif
 	
 	RS485_RX_EN;
 	gflagConnect = RESET;
@@ -372,32 +276,12 @@ int main(void)
 	while(1){
 		if( gflagConnect ){
 			gflagConnect = 0;
-
-			switch(gUartTxBuffer[1]){
-				case 'A':	// current
-					arg1 = 'A'; 
-					arg2 = adcResult[0];
-					rs485Send(arg1,arg2);
-					break;
-				case 'B':  // volt
-					arg1 = 'B'; 
-					arg2 = adcResult[1];
-					rs485Send(arg1,arg2);
-					break;
-				case 'C':  // press
-					arg1 = 'C'; arg2 = adcResult[2];
-					rs485Send(arg1,arg2);
-					break;
-				case 'D':	// digital input
-					rs485SendInput( );
-				break;
+			if(( gUartTxBuffer[2] == 'E') && (gUartTxBuffer[1] == ID_ADDR)){
+				rs485Send( );
 			}
 		} else {
 			RS485_TX_EN;
 		}
-		sensCurrent = calcSens(adcResult[0],currentScale,currentOffset);
-		sensVolt	= calcSens(adcResult[1],voltScale,voltOffset);
-		sensPress	= calcSens(adcResult[2],pressScale,pressOffset);
 	}
 }
 
